8fae6695d40b0ee58c79dd2d8e2bb047e1668866
Yassin-Aru <jocarhisouka@gmail.com>
2023-07-18 18:37:33 +0100

    more buggs in terminal

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
-
+import User from "@models/user";
 
 const handler = NextAuth({
     providers: [
@@ -11,19 +11,35 @@ const handler = NextAuth({
         })
     ],
     async session({session}){
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        });
 
+        session.user.id = sessionUser._id.toString();
+
+        return session;
     },
     async signIn({profile}) {
         try {
             await connectToDB();
 
             // check if the user is already exist
-
+            const userExists = await User.findOne({
+                email: profile.email
+            });
             // if not create new user and add it to DB
-
+            if(!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.pircture
+                })
+            }
             return;
         } catch (error) {
             console.log(error);
         }
     }
-})
+});
+
+export {handler as GET, handler as POST};
